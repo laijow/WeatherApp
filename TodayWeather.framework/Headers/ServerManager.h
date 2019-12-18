add41e1be29ccfe30af83dc2fc03730bd18e2597
Anatoly Yem <powerhostvlz@mail.ru>
2019-12-18 19:11:57 +1000

    removed singletone from server manager

--- TodayWeather.framework/Headers/ServerManager.h
+++ TodayWeather.framework/Headers/ServerManager.h
@@ -13,8 +13,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ServerManager : NSObject
 
-+ (ServerManager*)sharedManager;
-
 - (void)startRequestWithMethod:(NSString*)method
                    blockResult:(void(^)(id jsonResult, NSError * error)) blockResult;
 
